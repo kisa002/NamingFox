206240d8e893ec4c1b06edfb5792ca35f3be8720
Holy-Knight <vnycall74@naver.com>
2023-02-18 19:34:17 +0900

    Remove unused old model

--- plugin/src/main/kotlin/api/model/RequestEntity.kt
+++
@@ -1,6 +0,0 @@
-package api.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class RequestEntity(val model: String, val prompt: String, val temperature: Float, val max_tokens: Int)
